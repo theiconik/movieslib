8cc8bc9146e8fa62fe3b7f31150d7ec07b686554
theiconik <kumarujjawal51@gmail.com>
2021-05-20 16:47:36 +0530

    More customizations

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Add() {
    return (
-      <div>
-         <h1>Add New</h1>
+      <div className="add-page">
+         <div className="container">
+            <div className="add-content">
+               <div className="input-wrapper" style={{display:"flex",}}>
+                  <span className="btn" style={{
+                     marginRight:"-5px",
+                     cursor:"default",
+                  }}>Search</span>
+                  <input type="text" className="input-box"/>
+               </div>
+            </div>
+         </div>
       </div>
    )
 }
