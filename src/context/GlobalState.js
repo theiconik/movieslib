b231fa2b9558271e059a561bd9152f8e20445b8c
theiconik <kumarujjawal51@gmail.com>
2021-05-25 01:26:55 +0530

    storing watchlist in local storage

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,10 +1,14 @@
-import React, {createContext, useReducer, useEffect} from 'react'
-import AppReducer from './AppReducer'
+import React, { createContext, useReducer, useEffect } from "react";
+import AppReducer from "./AppReducer";
 
 //initial state
 const initialState = {
-   watchlist : [],
-   watched: [],
+  watchlist: localStorage.getItem("watchlist")
+    ? JSON.parse(localStorage.getItem("watchlist"))
+    : [],
+  watched: localStorage.getItem("watched")
+    ? JSON.parse(localStorage.getItem("watched"))
+    : [],
 };
 
 //create context
@@ -12,21 +16,28 @@ export const GlobalContext = createContext(initialState);
 
 //Provider Components
 
-export const GlobalProvider = props => {
-   const [state, dispatch] = useReducer(AppReducer, initialState)
+export const GlobalProvider = (props) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-   useEffect(() => {
-      localStorage.setItem('watchlist', JSON.stringify(state.watchlist))
-   }, [state]);
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(state.watchlist));
+    localStorage.setItem('watched', JSON.stringify(state.watched))
+  }, [state]);
 
-   //actions
-   const addMovieToWatchlist = movie => {
-      dispatch({type: "ADD_MOVIE_TO_WATCHLIST", payload: movie})
-   }
+  //actions
+  const addMovieToWatchlist = (movie) => {
+    dispatch({ type: "ADD_MOVIE_TO_WATCHLIST", payload: movie });
+  };
 
-   return (
-      <GlobalContext.Provider value = {{watchlist : state.watchlist, watched: state.watched, addMovieToWatchlist,}}>
-         {props.children}
-      </GlobalContext.Provider>
-   )
-}
+  return (
+    <GlobalContext.Provider
+      value={{
+        watchlist: state.watchlist,
+        watched: state.watched,
+        addMovieToWatchlist,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
