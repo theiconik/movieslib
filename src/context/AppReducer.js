e1243b6c5c975a1ef9b51c89f8db98a26174c259
theiconik <kumarujjawal51@gmail.com>
2021-05-29 23:26:36 +0530

    changed to arrow function

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const AppReducer = (state, action) => {
   switch (action.type) {
     case "ADD_MOVIE_TO_WATCHLIST":
       return {
@@ -38,4 +38,6 @@ export default (state, action) => {
     default:
       return state;
   }
-};
+}
+
+export default AppReducer;
